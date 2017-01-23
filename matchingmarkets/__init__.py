e883222b9f866508b64512fc9989a641294b2dfd
VHRanger <matthieu.ranger@outlook.com>
2017-01-23 11:46:31 -0500

    Update __init__.py

--- matchingmarkets/__init__.py
+++ matchingmarkets/__init__.py
@@ -1,15 +1,15 @@
-__all__ = ["agent", "algorithms", "market", "metaalgorithms",
-			"simulations", "utilities"]
-			
 import matchingmarkets.agent
 import matchingmarkets.algorithms
 import matchingmarkets.metaalgorithms
 import matchingmarkets.market
 import matchingmarkets.utilities
 import matchingmarkets.simulations
-from matchingmarkets.agent import Agent
-from matchingmarkets.utilities import rngDraw
-from matchingmarkets.metaalgorithms import meta_Greedy, meta_Patient
-from matchingmarkets.algorithms import arbitraryMatch, TTC
-from matchingmarkets.market import Market
+from matchingmarkets.agent import *
+from matchingmarkets.utilities import *
+from matchingmarkets.metaalgorithms import *
+from matchingmarkets.algorithms import *
+from matchingmarkets.market import *
 from matchingmarkets.simulations import simulation
+
+__all__ = ["agent", "algorithms", "market", "metaalgorithms",
+           "simulations", "utilities"]
