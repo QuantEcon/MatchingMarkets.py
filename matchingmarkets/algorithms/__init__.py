0b04d4d60085fb1adfa0ccc7827f323a438f05f1
Matt Ranger <matthieu.ranger@outlook.com>
2017-06-22 01:52:39 -0400

    Update __init__.py

--- matchingmarkets/algorithms/__init__.py
+++ matchingmarkets/algorithms/__init__.py
@@ -1,7 +1,8 @@
 from matchingmarkets.algorithms import *
 from matchingmarkets.algorithms.basic import *
 from matchingmarkets.algorithms.TTC import *
+from matchingmarkets.algorithms.DA import *
 from matchingmarkets.algorithms import pulp
 
 # Kidney Solvers not implmented yet
-__all__ = ["basic", "TTC", "pulp"] #, "kidney_solvers"]
+__all__ = ["basic", "TTC", "pulp", "DA"] #, "kidney_solvers"]
